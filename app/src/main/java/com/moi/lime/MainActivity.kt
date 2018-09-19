9eeb5b936b458302739d389418fd1acef6e9a30e
hxkang <hxkang@thoughtworks.com>
2018-09-19 17:27:59 +0800

    [ARCH]Fix logger bugs

--- app/src/main/java/com/moi/lime/MainActivity.kt
+++ app/src/main/java/com/moi/lime/MainActivity.kt
@@ -2,11 +2,14 @@ package com.moi.lime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.moi.lime.util.Logger
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Logger.INS.setUpLogger(BuildConfig.DEBUG,"Lime")
+        Logger.INS.d("ddudududu")
     }
 }
--- app/src/main/java/com/moi/lime/util/Logger.kt
+++ app/src/main/java/com/moi/lime/util/Logger.kt
@@ -3,40 +3,39 @@ package com.moi.lime.util
 import android.util.Log
 
 enum class Logger {
-    INS {
-        private var isDebug = true
-        private var tag = "Logger"
-        private var isTest = false
-        fun setupLogger(isDebug: Boolean, tag: String, isTest: Boolean = false) {
-            this.isDebug = isDebug
-            this.tag = tag
-            this.isTest = isTest
-        }
+    INS;
+    private var isDebug = true
+    private var tag = "Logger"
+    private var isTest = false
+    fun setUpLogger(isDebug: Boolean, tag: String, isTest: Boolean = false) {
+        this.isDebug = isDebug
+        this.tag = tag
+        this.isTest = isTest
+    }
 
-        fun d(message: String) {
-            check {
-                Log.d(tag, message)
-            }
+    fun d(message: String) {
+        check {
+            Log.d(tag, message)
         }
+    }
 
-        fun i(message: String) {
-            check {
-                Log.i(tag, message)
-            }
+    fun i(message: String) {
+        check {
+            Log.i(tag, message)
         }
+    }
 
-        fun e(message: String) {
-            check {
-                Log.e(tag, message)
-            }
+    fun e(message: String) {
+        check {
+            Log.e(tag, message)
         }
+    }
 
-        private fun check(block: () -> Unit) {
-            if (isDebug && !isTest) {
-                block()
-            }
+    private fun check(block: () -> Unit) {
+        if (isDebug && !isTest) {
+            block()
         }
-    };
+    }
 
 
 }
