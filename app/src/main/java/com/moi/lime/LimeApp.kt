1661b69ac6e855172a2f9637307a5b3e69d95414
hxkang <hxkang@thoughtworks.com>
2018-09-19 17:47:35 +0800

    [ARCH]Start to integrate dagger

---
+++ app/src/main/java/com/moi/lime/LimeApp.kt
@@ -0,0 +1,6 @@
+package com.moi.lime
+
+import android.app.Application
+
+class LimeApp:Application() {
+}
---
+++ app/src/main/java/com/moi/lime/di/AppInjector.kt
@@ -0,0 +1,36 @@
+package com.moi.lime.di
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.moi.lime.LimeApp
+import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.HasSupportFragmentInjector
+
+object AppInjector {
+    fun init(limeApp: LimeApp){
+    }
+    private fun handleActivity(activity: Activity) {
+        if (activity is HasSupportFragmentInjector) {
+            AndroidInjection.inject(activity)
+        }
+        if (activity is FragmentActivity) {
+            activity.supportFragmentManager
+                    .registerFragmentLifecycleCallbacks(
+                            object : FragmentManager.FragmentLifecycleCallbacks() {
+                                override fun onFragmentCreated(
+                                        fm: FragmentManager,
+                                        f: Fragment,
+                                        savedInstanceState: Bundle?) {
+                                    if (f is Injectable) {
+                                        AndroidSupportInjection.inject(f)
+                                    }
+                                }
+                            }, true
+                    )
+        }
+    }
+}
