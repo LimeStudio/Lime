1b3b150035a2f301c744700e29d6ca5e97e5db1d
hxkang <hxkang@thoughtworks.com>
2019-02-24 17:54:51 +0800

    [UI] Add basic layout for recommend page

--- app/src/main/java/com/moi/lime/ui/home/recommend/RecommendFragment.kt
+++ app/src/main/java/com/moi/lime/ui/home/recommend/RecommendFragment.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.moi.lime.R
+import com.moi.lime.databinding.FragmentRecommendBinding
 
 class RecommendFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+        val dataBinding = DataBindingUtil.inflate<FragmentRecommendBinding>(
+                inflater,
+                R.layout.fragment_recommend,
+                container,
+                false
+        )
+        dataBinding.recommendViewPager.offscreenPageLimit = 1
+        dataBinding.adapter = RecommendViewPagerAdapter(childFragmentManager, listOf("", "", "", "", ""))
+        return dataBinding.root
     }
 }
---
+++ app/src/main/java/com/moi/lime/ui/home/recommend/RecommendViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.moi.lime.ui.home.recommend
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class RecommendViewPagerAdapter(
+        fragmentManager: FragmentManager,
+        dataList: List<String>
+) : FragmentStatePagerAdapter(fragmentManager) {
+
+    private val fragments = dataList.map { RecommendItemFragment() }
+
+    override fun getItem(position: Int) = fragments[position]
+
+    override fun getCount() = fragments.size
+}
