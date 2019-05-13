f3ec910e28b0a762eedf9da801ef1ac464905fa9
hxkang <hxkang@thoughtworks.com>
2019-05-13 17:15:09 +0800

    [UT] Add unit testing for SignInViewModel

--- app/src/main/java/com/moi/lime/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/moi/lime/ui/signin/SignInViewModel.kt
@@ -2,10 +2,12 @@ package com.moi.lime.ui.signin
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
+import com.lime.testing.OpenForTesting
 import com.moi.lime.core.livedata.SingleLiveEvent
 import com.moi.lime.repository.LimeRepository
 import javax.inject.Inject
 
+@OpenForTesting
 class SignInViewModel @Inject constructor(private val limeRepository: LimeRepository) : ViewModel() {
 
     val loginInfo = SingleLiveEvent<Pair<String, String>>()
---
+++ app/src/test/java/com/moi/lime/ui/signin/SignInViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.moi.lime.ui.signin
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.moi.lime.repository.LimeRepository
+import com.moi.lime.util.mock
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito.verify
+
+@RunWith(JUnit4::class)
+class SignInViewModelTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val limeRepository = mock<LimeRepository>()
+
+    @Test
+    fun testCallSignIn(){
+        val viewModel = SignInViewModel(limeRepository)
+        viewModel.loginResource.observeForever { mock() }
+        viewModel.loginInfo.value = Pair("test","test")
+        verify(limeRepository).signIn("test","test")
+    }
+}
