d1028bf8b8e64de327a373f88bede49d2bbadb6b
大冰 <469142634@qq.com>
2019-10-24 16:28:55 +0800

    modify ci AND configuration from 28 to 29

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,32 +28,6 @@ allprojects {
         }
     }
 }
-
-task("jacocoUnitTestReport", JacocoReport::class) {
-    dependsOn("testDebugUnitTest")
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = true
-    }
-    val fileFilter = arrayOf("**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*", "**/*Test*.*", "android/**/*.*")
-    val debugTree = listOf(
-            fileTree("$buildDir/intermediates/classes/debug").apply {
-                exclude(*fileFilter)
-            },
-            fileTree("$buildDir/tmp/kotlin-classes/debug").apply {
-                exclude(*fileFilter)
-            }
-    )
-    val mainSrc = "$projectDir/src/main/java"
-    sourceDirectories.from(mainSrc)
-    classDirectories.from(debugTree)
-    executionData.from(fileTree(projectDir).apply {
-        include("build/jacoco/testDebugUnitTest.exec")
-    })
-
-
-}
-
 //
 //task clean(type: Delete) {
 //    delete rootProject.buildDir
