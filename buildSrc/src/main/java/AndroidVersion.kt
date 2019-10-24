91d305ad6441940e6732e28446d84494119e3531
大冰 <469142634@qq.com>
2019-10-24 17:10:20 +0800

    modify MIN_SDK_VERSION from 20 to 21

--- buildSrc/src/main/java/AndroidVersion.kt
+++ buildSrc/src/main/java/AndroidVersion.kt
@@ -1,6 +1,6 @@
 object AndroidVersion {
     const val COMPILE_VERSION = 29
-    const val MIN_SDK_VERSION = 20
+    const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 29
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
