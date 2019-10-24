338a5f1432ab227a78565ba6aacb52878da5f2d1
大冰 <469142634@qq.com>
2019-10-24 17:18:59 +0800

    modify COMPILE_VERSION from 29 to 28

--- buildSrc/src/main/java/AndroidVersion.kt
+++ buildSrc/src/main/java/AndroidVersion.kt
@@ -1,7 +1,7 @@
 object AndroidVersion {
-    const val COMPILE_VERSION = 29
+    const val COMPILE_VERSION = 28
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 29
+    const val TARGET_SDK_VERSION = 28
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 }
