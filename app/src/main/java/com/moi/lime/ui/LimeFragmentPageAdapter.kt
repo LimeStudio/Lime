cf01af159b057449ef131ebcb9712c3f86b49319
hxkang <hxkang@thoughtworks.com>
2020-03-04 16:12:19 +0800

    Add behavior to FragmentPagerAdapter

--- app/src/main/java/com/moi/lime/ui/LimeFragmentPageAdapter.kt
+++ app/src/main/java/com/moi/lime/ui/LimeFragmentPageAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class LimeFragmentPageAdapter(fragmentManager: FragmentManager, private val fragmentList: List<Fragment>) : FragmentPagerAdapter(fragmentManager) {
+class LimeFragmentPageAdapter(fragmentManager: FragmentManager, private val fragmentList: List<Fragment>) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int) = fragmentList[position]
 
     override fun getCount() = fragmentList.size
